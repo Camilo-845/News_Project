122181597abf08687db4bb027336f3fca94c558f
Camilo S <camilo.sarmiento.amado@gmail.com>
2023-05-11 18:29:25 -0500

    routes and components files configuration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,26 @@
+import React from 'react';
 import { useState } from 'react'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import LandingPage from './views/LandingPage/LandingPage';
+import Home from './views/Home/Home';
+import NotFound from './views/NotFound/NotFound';
 
 import './App.css'
 
 function App() {
-  
+    return (
+        <Router>
+            <Switch>
+                <Route exact path='/' component={LandingPage} />
+                <>
+                    <Switch>
+                        <Route path="/home" component={Home} />
+                        <Route path="*" component={NotFound} />
+                    </Switch>
+                </>
+            </Switch>
+        </Router>
+    )
 }
 
 export default App
