b6390b72a3e32132a988546943a4ee145567fe56
Camilo S <camilo.sarmiento.amado@gmail.com>
2023-05-12 19:24:39 -0500

    Landing Page header HTML and CSS

--- frontend/src/views/Home/Home.jsx
+++ frontend/src/views/Home/Home.jsx
@@ -1,10 +1,9 @@
 import React from "react";
+import styles from "./Home.module.css"
 
-function Home (){
+function Home() {
     return (
-        <div>
-            
-        </div>
+        <></>
     )
 }
 
--- frontend/src/views/LandingPage/LandingPage.jsx
+++ frontend/src/views/LandingPage/LandingPage.jsx
@@ -1,10 +1,37 @@
 import React from "react";
+import styles from "./LandingPage.module.css"
 
-function LandingPage(){
+function LandingPage() {
     return (
-        <div>
-            <h1>HOLA q ase</h1>
-        </div>
+        <article className={styles.mainContainer}>
+            <header className={styles.headerContainer}>
+                <figure>
+                    <a className={styles.logo} href="/">
+                        <h1>AT!</h1>
+                    </a>
+                </figure>
+                <section className={styles.signButtons}>
+                    <button>
+                        <h1 className={styles.login}>LogIn</h1>
+                    </button>
+                    <button>
+                        <h1 className={styles.signup}>SignUp</h1>
+                    </button>
+                </section>
+            </header>
+            <main>
+                <article>
+
+                </article>
+                <article>
+                    <figure>
+
+                    </figure>
+                    <h2></h2>
+                    <p></p>
+                </article>
+            </main>
+        </article>
     )
 }
 
