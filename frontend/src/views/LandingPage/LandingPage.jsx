87b22c2494200df4ea9175f15d2c9a301bef0579
Camilo S <camilo.sarmiento.amado@gmail.com>
2023-08-21 19:57:50 -0500

    dev: landing footer

--- frontend/src/views/LandingPage/LandingPage.jsx
+++ frontend/src/views/LandingPage/LandingPage.jsx
@@ -71,7 +71,33 @@ function LandingPage() {
                 <NewsSection size="small" data={datosPruebaNewsSection}></NewsSection>
             </article>
             <footer className={styles.footer}>
+                <section className={styles.footerLinks}>
+                    <article>
+                        <a href="#"><p>About</p></a>
+                        <a href="#"><p>Contact</p></a>
+                        <a href="#"><p>Frequently Asked Questions</p></a>
+                        <a href="#"><p>Terms and Conditions</p></a>
+                    </article>
+                    <article>
+                        <a href="https://www.linkedin.com/in/camilo-sarmiento-051a80244/" target="_blank"><p>LinkedIn</p></a>
+                        <a href="#"><p>Twitter</p></a>
+                        <a href="https://github.com/Camilo-845" target="_blank"><p>GitHub</p></a>
+                        <a href="#"><p>Instagram</p></a>
+                    </article>
+                </section>
+                <section className={styles.footerInfo}>
+                    <article>
+                        <a className={styles.logoFooter} href="/">
+                            <h1>AT!</h1>
+                        </a>
+                        <p>© Actuality  2023</p>
+                    </article>
+                    <article>
+                        <p className={styles.tagDesingBy}>Design by</p>
+                        <p>Camilo Sarmiento</p>
+                    </article>
 
+                </section>
             </footer>
         </article>
     )
