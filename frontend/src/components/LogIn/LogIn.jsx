f6e7a921636c08eadc863dfc6c865cb61d3285d2
Camilo S <camilo.sarmiento.amado@gmail.com>
2023-08-24 01:05:21 -0500

    feat: LogIn CSS

--- frontend/src/components/LogIn/LogIn.jsx
+++ frontend/src/components/LogIn/LogIn.jsx
@@ -9,25 +9,30 @@ function LogIn (){
                 <section className={styles.mainText}>
                     <h2>Welcome to</h2>
                     <br />
-                    <h1>AT!</h1>
-                    <h2>Actuallity</h2>
+                    <section>
+                        <h1>AT!</h1>
+                        <h2>Actuallity</h2>
+                    </section>
                 </section>
                 <section className={styles.formContainer}>
                     <h2>Log in to your account</h2>
                     <br />
                     <form action="">
-                        <h2>Email Address</h2>
-                        <input type="text" />
+                        <section>
+                            <h2>Email Address</h2>
+                            <input type="text" />
+                            <br />
+                            <h2>Password</h2>
+                            <input type="text" />
+                        </section>
                         <br />
-                        <h2>Password</h2>
-                        <input type="text" />
                         <br />
-                        <input type="submit" />
+                        <input value={"LogIn"} type="submit" />
                     </form>
                     <a href="/signup"><p>Don't have an account?</p></a>
                 </section>
                 <a className={styles.exitButton} href="/">
-                    <button>X</button>
+                    <button>x</button>
                 </a>
             </article>
         </article>
