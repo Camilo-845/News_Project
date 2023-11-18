3e94e7956e51cab5d4cee3cd169a120360e780ae
Camilo S <camilo.sarmiento.amado@gmail.com>
2023-11-18 16:18:41 -0500

    NewWideSection Component HTMl and CSS

--- frontend/src/components/NewWideSection/NewWideSection.jsx
+++ frontend/src/components/NewWideSection/NewWideSection.jsx
@@ -1,9 +1,31 @@
 import React from "react";
-
-function NewWideSection({name}){
+import styles from "./NewWideSection.module.css"
+function NewWideSection({data}){
+    const landing = false;
+    const defaultData = {
+        id: (data &&data.hasOwnProperty(id))?data.id:"",
+        title: (data &&data.hasOwnProperty(title))?data.title:"",
+        description: (data &&data.hasOwnProperty(description))?data.description.slice(0,150):"",
+        url: (data &&data.hasOwnProperty(url))?data.url:"",
+        author: (data &&data.hasOwnProperty(author))?`Por:${ data.author}`:"",
+        image: (data &&data.hasOwnProperty(image))?data.image:" ",
+        lenguage: (data &&data.hasOwnProperty(lenguage))?data.lenguage:"",
+        category:[
+            (data &&data.hasOwnProperty(category))?data.category:"",
+        ],
+        published:(data && data.published)?data.published:""
+    }
     return(
-        <article>
-            <h1>{name}</h1>
+        <article className={styles.mainContainer}>
+            <p className={landing?styles.published_Date_Landing:styles.published_Date}>{defaultData.published}</p>
+            <article className={landing?styles.newTextContiner_Landing:styles.newTextContiner}>
+                <h2>{defaultData.title}</h2>
+                <p>{defaultData.description}</p>
+                <p>{defaultData.author}</p>
+            </article>
+            <picture className={styles.imgageContainer}>
+                <img src={defaultData.image} alt="" />
+            </picture>
         </article>
     )
 }
