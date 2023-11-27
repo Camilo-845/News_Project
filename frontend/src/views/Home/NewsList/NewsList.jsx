c600405df810388f2332fba74f4b18a4b2790f67
Camilo S <camilo.sarmiento.amado@gmail.com>
2023-11-27 12:48:51 -0500

    HTML y CSS filtro en componente NewsList

--- frontend/src/views/Home/NewsList/NewsList.jsx
+++ frontend/src/views/Home/NewsList/NewsList.jsx
@@ -1,9 +1,29 @@
 import React from "react";
 import NewWideSection from "../../../components/NewWideSection/NewWideSection";
-
+import styles from "./NewsList.module.css"
+import filterIcon from "../../../assets/setting-5-svgrepo-com.svg"
 function NewsList({data}){
+
+    const categories = [];
     return (
         <section>
+            <section className={styles.filterContainer}>
+                <picture>
+                    <img src={filterIcon} alt="Filter settings logo" />
+                </picture>
+                <article className={styles.filtros}>
+                    <div>
+                        <h3>Category</h3>
+                        <select name="category" id="all">
+                            <option value="">Select</option>
+                            {categories?.map(category => {
+                                return (<option value={`${category.id}`} key={category.id}>{category.name.toUpperCase()}</option>
+                                )
+                            })}
+                        </select>
+                    </div>
+                </article>
+            </section>
             {data?data.map((element) => {
                 return (
                     <article key={element.id}>
