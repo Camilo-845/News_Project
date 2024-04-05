522ef28f9f46dfdde1e4442ccdcdc827512e302a
Camilo S <camilo.sarmiento.amado@gmail.com>
2024-04-04 19:59:34 -0500

    fix: action GetNewsByPage bug fixed

--- frontend/src/redux/actions/index.js
+++ frontend/src/redux/actions/index.js
@@ -1,5 +1,17 @@
+export const BACKEND_URL="http://localhost:3001/"
+
+export const GET_NEWS_BY_PAGE = "GET_NEWS_BY_PAGE"
 
 //Actions: 
+export const getNewsByPage = (page)=>{
+    return function(dispatch){
+        return fetch(`${BACKEND_URL}news?page=${typeof(page) != "number"? 0 : page}`)
+        .then((response)=>["200","304"].includes(response.status) ? [] : response.json())
+        .then(data=>{
+            dispatch({type:GET_NEWS_BY_PAGE, payload: data, status: data.status})
+        })
+    }
+}
 //News:
     //Get News by page
     //Get News by Id
--- frontend/src/redux/reducer/index.js
+++ frontend/src/redux/reducer/index.js
@@ -1,10 +1,25 @@
+import { GET_NEWS_BY_PAGE } from "../actions";
+
+
 const initialState = {
+  news:{
+    page:0,
+    content:[]
+  }
 }
 
 
 
 const rootReducer = (state = initialState, action) => {
     switch (action.type) {
+      case GET_NEWS_BY_PAGE:
+        return {
+          ...state,
+          news:{
+            page: ["200","304"].includes(action.status)?state.news.page :(state.news.page + 1),
+            content : action.payload
+          }
+        }
       default:return {...state}
     }
   };
